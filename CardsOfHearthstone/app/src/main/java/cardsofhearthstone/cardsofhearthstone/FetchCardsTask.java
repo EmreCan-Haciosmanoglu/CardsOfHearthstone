8d924b75bd69790371684e84303191590a762c35
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2018-11-25 04:21:06 +0300

    new api

--- CardsOfHearthstone/app/src/main/java/cardsofhearthstone/cardsofhearthstone/FetchCardsTask.java
+++ CardsOfHearthstone/app/src/main/java/cardsofhearthstone/cardsofhearthstone/FetchCardsTask.java
@@ -3,10 +3,12 @@ package cardsofhearthstone.cardsofhearthstone;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.json.*;
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -43,114 +45,124 @@ public class FetchCardsTask extends AsyncTask<String, Void, String[]> {
             urlConnection.setRequestMethod("GET");
             urlConnection.connect();
 
+            /////////////////////////////////////////////////////////
 
+            HttpResponse<JsonNode> response;
+            response = Unirest.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards?attack=5&cost=3")
+                    .header("X-Mashape-Key", "3BNNcA7YhAmshdhzxe2TulPnRS02p1OV7wQjsn3v2ADuMrMatn")
+                    .asJson();
+
+            Log.v("json", response.getBody().toString());
+
+            ///////////////////////////////////
 
             InputStream inputStream = urlConnection.getInputStream();
             StringBuffer buffer     = new StringBuffer();
 
-            if (inputStream != null) {
+            if (inputStream != null)
+            {
                 reader = new BufferedReader(new InputStreamReader(inputStream));
 
                 String line;
-                while ((line = reader.readLine()) != null) {
+                while ((line = reader.readLine()) != null)
+                {
                     buffer.append(line + "\n");
                 }
-                if (buffer.length() != 0) {
+                if (buffer.length() != 0)
+                {
                     cardData = buffer.toString();
-
-
                 }
             }
-        } catch (IOException e) {
+        }
+        catch (Exception e)
+        {
             Log.e("MainActivity", "Error ", e);
-        } finally{
-            if (urlConnection != null) {
+        }
+        finally
+        {
+            if (urlConnection != null)
+            {
                 urlConnection.disconnect();
             }
-            if (reader != null) {
-                try {
+            if (reader != null)
+            {
+                try
+                {
                     reader.close();
-                } catch (final IOException e) {
+                }
+                catch (final IOException e)
+                {
                     Log.e("MainActivity", "Error closing stream", e);
                 }
             }
         }
-
-        try {
-
+        try
+        {
             return getWeatherDataFromJson(cardData);
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             Log.e("FetchWeatherTask", e.getMessage(), e);
         }
-
-        // This will only happen if there was an error getting or parsing the forecast.
-
         return null;
-
-
     }
 
     private String[] getWeatherDataFromJson(String forecastJsonStr)  {
         Log.v("HACIOSMANOGLU",forecastJsonStr);
+
         JSONArray mainArray= null;
-        try {
+        try
+        {
             mainArray = new JSONArray(cardData);
-        } catch (JSONException e) {
+        }
+        catch (JSONException e)
+        {
             e.printStackTrace();
         }
         Log.v("carddata", String.valueOf(mainArray.length()));
-        for(int i=0;i<mainArray.length();i++){
+        for(int i=0;i<mainArray.length();i++)
+        {
             String cardType=null;
             JSONObject card=null;
-            try {
+            try
+            {
                 card=mainArray.getJSONObject(i);
                 cardType=card.getString("type");
-
-            } catch (Exception e){
-
             }
+            catch (Exception e) {}
 
-
-            if(cardType.equals("HERO")){
+            if(cardType.equals("HERO"))
+            {
                 HeroCards Card=new HeroCards();
-                 try {
+                try
+                {
                     Card.setCost(card.getInt("cost"));
-
-                } catch (Exception e){
+                }
+                catch (Exception e)
+                {
                     Card.setCost(0);
                 }
-                try {
+                try
+                {
                     Card.setID(card.getString("id"));
                     Card.setName(card.getString("name"));
-
-
-
                     Card.setCardClass(card.getString("cardClass"));
-
                     Card.setRarity(card.getString("rarity"));
                     Card.setArmor(card.getInt("armor"));
                     Card.setCollectible(card.getBoolean("collectible"));
                     Card.setHealth(card.getInt("health"));
-
-                } catch (Exception e){
-
                 }
-
-
-
+                catch (Exception e){ }
 
                 heroCards[heroIndex]=Card;
                 heroIndex++;
                 numberofCards++;
-
             }
-
-
-
-            try {
-                if(cardType.equals("SPELL")){
+            try
+            {
+                if(cardType.equals("SPELL"))
+                {
                     SpellCards Card=new SpellCards();
-
 
                     Card.setCardClass(card.getString("cardClass"));
                     Card.setCost(card.getInt("cost"));
@@ -158,39 +170,34 @@ public class FetchCardsTask extends AsyncTask<String, Void, String[]> {
                     Card.setID(card.getString("id"));
                     Card.setCollectible(card.getBoolean("collectible"));
                     Card.setName(card.getString("name"));
-
 
                     spellCards[spellIndex]=Card;
                     spellIndex++;
                     numberofCards++;
-
-
                 }
-                if(cardType.equals("MINION")){
+                if(cardType.equals("MINION"))
+                {
                     MinionCards Card=new MinionCards();
 
                     Card.setID(card.getString("id"));
                     Card.setName(card.getString("name"));
-
                     Card.setCardClass(card.getString("cardClass"));
                     Card.setCost(card.getInt("cost"));
                     Card.setRarity(card.getString("rarity"));
-
                     Card.setCollectible(card.getBoolean("collectible"));
                     Card.setHealth(card.getInt("health"));
                     Card.setAttack(card.getInt("attack"));
 
-
                     minionCards[minionIndex]=Card;
                     minionIndex++;
                     numberofCards++;
-
                 }
-                if(cardType.equals("WEAPON")){
+                if(cardType.equals("WEAPON"))
+                {
                     WeaponCards Card=new WeaponCards();
+
                     Card.setID(card.getString("id"));
                     Card.setName(card.getString("name"));
-
                     Card.setCardClass(card.getString("cardClass"));
                     Card.setCost(card.getInt("cost"));
                     Card.setRarity(card.getString("rarity"));
@@ -198,38 +205,30 @@ public class FetchCardsTask extends AsyncTask<String, Void, String[]> {
                     Card.setAttack(card.getInt("attack"));
                     Card.setDurability(card.getInt("durability"));
 
-
                     weaponCards[weaponIndex]=Card;
                     weaponIndex++;
                     numberofCards++;
-
                 }
-
-            } catch (Exception e){
+            }
+            catch (Exception e)
+            {
                 e.printStackTrace();
             }
-
-
         }
-
-
-
-
         return new String[]{"fsaf"};
     }
 
     @Override
     protected void onPostExecute(String[] aString) {
         super.onPostExecute(aString);
+
+        /*
         Log.v("carddata","finished");
         Log.v("caddata",String.valueOf(heroIndex));
         Log.v("caddata",String.valueOf(minionIndex));
         Log.v("caddata",String.valueOf(spellIndex));
         Log.v("caddata",String.valueOf(weaponIndex));
-
         Log.v("carddata",String.valueOf(numberofCards));
-
-
-
+        */
     }
 }
